b2e3413bad90218ae1e14d6c3c62c577b2666149
stuarthua <stuarthua.cn@gmail.com>
2020-03-17 02:55:27 +0800

    repost by shell.

--- scripts/build_by_quick.sh
+++ scripts/build_by_quick.sh
@@ -186,7 +186,7 @@ add_packages2feeds
 do_pre_feeds(){
     echo "update/install feeds..."
     cd $sdk_path
-    rm -rf feeds/awesome*
+    # rm -rf feeds/awesome*
     ./scripts/feeds update -a && ./scripts/feeds install -a
     # ./scripts/feeds update awesome && ./scripts/feeds install -a -p awesome
     echo -e "$INFO update/install feeds done!"
