783bca2bae0475ef062c1b9aa1487a2ebbf3accf
stuarthua <stuarthua.cn@gmail.com>
2020-06-11 13:43:26 +0800

    repost by shell.

--- scripts/build_by_quick.sh
+++ scripts/build_by_quick.sh
@@ -287,38 +287,38 @@ edit_config(){
 }
 edit_config
 
-# make download
-do_make_download(){
-    cd $sdk_path
-    if [ -d dl ]; then
-        # @https://p3terx.com/archives/openwrt-compilation-steps-and-commands.html
-        # 查找 dl 目录下文件是否下载正常，小于 1k 的文件，说明下载可能不完整
-        result=`find dl -size -1024c -exec ls -l {} \;`
-        if [ -n "$result" ]; then
-            # 删除 dl 目录下小于 1k 的文件
-            find dl -size -1024c -exec rm -f {} \;
-            make download -j8 V=s
-        else
-            echo "make download already done!"
-        fi
-    else
-        make download -j8 V=s
-    fi
-}
-download_dep(){
-    cd $sdk_path
-    while true; do
-        echo -n -e "$INPUT"
-        read -p "是否下载编译依赖 (y/n) ? " yn
-        echo
-        case $yn in
-            [Yy]* ) do_make_download; break;;
-            [Nn]* | "" ) break;;
-            * ) echo "输入 y 或 n 以确认";;
-        esac
-    done
-}
-download_dep
+# # make download
+# do_make_download(){
+#     cd $sdk_path
+#     if [ -d dl ]; then
+#         # @https://p3terx.com/archives/openwrt-compilation-steps-and-commands.html
+#         # 查找 dl 目录下文件是否下载正常，小于 1k 的文件，说明下载可能不完整
+#         result=`find dl -size -1024c -exec ls -l {} \;`
+#         if [ -n "$result" ]; then
+#             # 删除 dl 目录下小于 1k 的文件
+#             find dl -size -1024c -exec rm -f {} \;
+#             make download -j8 V=s
+#         else
+#             echo "make download already done!"
+#         fi
+#     else
+#         make download -j8 V=s
+#     fi
+# }
+# download_dep(){
+#     cd $sdk_path
+#     while true; do
+#         echo -n -e "$INPUT"
+#         read -p "是否下载编译依赖 (y/n) ? " yn
+#         echo
+#         case $yn in
+#             [Yy]* ) do_make_download; break;;
+#             [Nn]* | "" ) break;;
+#             * ) echo "输入 y 或 n 以确认";;
+#         esac
+#     done
+# }
+# download_dep
 
 ######################## build ########################
 
